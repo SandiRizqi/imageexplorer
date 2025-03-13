aa30e9b87528a14430cbf351609b4d1bb6bdf365
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-03-13 20:55:27 +0700

    add edge const

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import { authOptions } from "../../../components/auth/auth"
 
+// Add this line to enable Edge Runtime
+export const runtime = 'edge'
 
 // Export the handler functions using the App Router pattern
 const handler = NextAuth(authOptions)
